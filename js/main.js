c0738f64b1636878f25b852a543ad532bf4e8166
Ydie <ydie722@wp.pl>
2022-11-21 19:49:55 +0100

    finished site

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ const footerYear = document.querySelector(".footer__year");
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
 
+	navBtnBars.classList.remove("black-bars-color");
+
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
 			nav.classList.remove("nav--active");
@@ -38,5 +40,24 @@ const handleCurrentYear = () => {
 };
 handleCurrentYear(); // Wywołanie funkcji
 // Funkcja Roku w spanie footer
+
+// Obserwer
+const handleObserver = () => {
+	const curentSection = window.scrollY;
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= curentSection + 60
+		) {
+			navBtnBars.classList.add("black-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= curentSection + 60
+		) {
+			navBtnBars.classList.remove("black-bars-color");
+		}
+	});
+};
+window.addEventListener("scroll", handleObserver); //wywoałanie funkcji
 
 navBtn.addEventListener("click", handleNav);
